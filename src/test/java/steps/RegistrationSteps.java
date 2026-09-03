fe48fc6e127a06e6fc7ef3061e12b963d2a421cb
Marcos <marcos.souza26@outlook.com>
2026-09-03 19:36:19 -0300

    initial structure commit.

---
+++ src/test/java/steps/RegistrationSteps.java
@@ -0,0 +1,80 @@
+package steps;
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import pages.LoginPage;
+import org.junit.jupiter.api.Assertions;
+
+public class RegistrationSteps {
+
+    @Given("i click on the signup button for register")
+    public void i_click_on_the_signup_button_for_register() {
+
+    }
+
+    @When("i register my credentials")
+    public void i_register_my_credentials() {
+
+    }
+
+    @Then("i get the signup completed")
+    public void i_get_the_signup_completed() {
+
+    }
+
+    @Given("i click on the login button")
+    public void i_click_on_the_login_button() {
+
+    }
+
+    @Then("i get the login completed")
+    public void i_get_the_login_completed() {
+
+    }
+
+    @Given("i click on the login button to login")
+    public void i_click_on_the_login_button_to_login() {
+
+    }
+
+    @When("i register wrong credentials")
+    public void i_register_wrong_credentials() {
+
+    }
+
+    @Then("i get a warning to correct email and password")
+    public void i_get_a_warning_to_correct_email_and_password() {
+
+    }
+
+    @Given("i am already logged")
+    public void i_am_already_logged() {
+
+    }
+
+    @When("i click on the logout button")
+    public void i_click_on_the_logout_button() {
+    }
+
+    @Then("i get successfully logged out")
+    public void i_get_successfully_logged_out() {
+
+    }
+
+    @Given("i click on the signup button")
+    public void i_click_on_the_signup_button() {
+
+    }
+
+    @When("i register an already used email")
+    public void i_register_an_already_used_email() {
+
+    }
+
+    @Then("i get an error for the email")
+    public void i_get_an_error_for_the_email() {
+
+    }
+}
+
